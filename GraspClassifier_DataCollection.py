9a536ac54cfbc9e123da5c80bd145754390067a3
龙泉三尺剑 <jiaojiao@jiaojiaodebijibendiannao.local>
2024-12-02 09:57:09 +0000

    Update urdf loading path

--- GraspClassifier_DataCollection.py
+++ GraspClassifier_DataCollection.py
@@ -24,7 +24,7 @@ data = {'Ori_x': [],  # gripper orientation(base)
 # ----------------------------------Define functions------------------------------------
 
 
-def random_position(object_id,where):
+def random_position(object_id, where):
     # Generate random position around object to grasp
     aabb = p.getAABB(object_id)
     min_bound, max_bound = aabb
@@ -32,63 +32,64 @@ def random_position(object_id,where):
     print(f'Object\'s Max Bound: {max_bound}')
 
     if where == 0:
-      # noise for above(0)
-      noise_1 = random.gauss(mu=0, sigma=0.3)*0.1  # x noise
-      noise_2 = random.gauss(mu=0, sigma=0.3)*0.1  # y noise
-      noise_3 = random.gauss(mu=0.25, sigma=0.1)  # z noise
-      # Generate random position above the object (0)
-      x_safe = (max_bound[0]+min_bound[0])/2+noise_1
-      y_safe = (max_bound[1]+min_bound[1])/2+noise_2
-      z_safe = max_bound[2]+noise_3
-      
+        # noise for above(0)
+        noise_1 = random.gauss(mu=0, sigma=0.3)*0.1  # x noise
+        noise_2 = random.gauss(mu=0, sigma=0.3)*0.1  # y noise
+        noise_3 = random.gauss(mu=0.25, sigma=0.1)  # z noise
+        # Generate random position above the object (0)
+        x_safe = (max_bound[0]+min_bound[0])/2+noise_1
+        y_safe = (max_bound[1]+min_bound[1])/2+noise_2
+        z_safe = max_bound[2]+noise_3
+
     elif where == 1:
-      #noise for side(1)
-      noise_1 = random.gauss(mu=0.2, sigma=0.1) #x noise
-      noise_2 = random.gauss(mu=0, sigma=0.4)*0.1 #y noiseA
-      noise_3 = random.gauss(mu=0.25, sigma=0.1)*0.1   #z noise
-      #Generate random position from the side(1)
-      x_safe = min_bound[0]-noise_1
-      y_safe = (max_bound[1]+min_bound[1])/2+noise_2
-      z_safe = (max_bound[2]+min_bound[2])/2+noise_3
+        # noise for side(1)
+        noise_1 = random.gauss(mu=0.2, sigma=0.1)  # x noise
+        noise_2 = random.gauss(mu=0, sigma=0.4)*0.1  # y noiseA
+        noise_3 = random.gauss(mu=0.25, sigma=0.1)*0.1  # z noise
+        # Generate random position from the side(1)
+        x_safe = min_bound[0]-noise_1
+        y_safe = (max_bound[1]+min_bound[1])/2+noise_2
+        z_safe = (max_bound[2]+min_bound[2])/2+noise_3
 
     elif where == 2:
-      #noise for side(2)
-      noise_1 = random.gauss(mu=0, sigma=0.3)*0.1 #x noise
-      noise_2 =  random.gauss(mu=0.2, sigma=0.1) #y noiseA
-      noise_3 = random.gauss(mu=0.2, sigma=0.1)*0.1   #z noise  (if gripper in contact with the table,it will deform ,so I set Z-noise positive)
-      #Generate random position from the side(2)
-      x_safe =    (max_bound[0]+min_bound[0])/2+noise_1
-      y_safe = min_bound[1]-noise_2
-      z_safe = (max_bound[2]+min_bound[2])/2+noise_3
-      
+        # noise for side(2)
+        noise_1 = random.gauss(mu=0, sigma=0.3)*0.1  # x noise
+        noise_2 = random.gauss(mu=0.2, sigma=0.1)  # y noiseA
+        # z noise  (if gripper in contact with the table,it will deform ,so I set Z-noise positive)
+        noise_3 = random.gauss(mu=0.2, sigma=0.1)*0.1
+        # Generate random position from the side(2)
+        x_safe = (max_bound[0]+min_bound[0])/2+noise_1
+        y_safe = min_bound[1]-noise_2
+        z_safe = (max_bound[2]+min_bound[2])/2+noise_3
+
     elif where == 3:
-      # noise for side(3)
-      noise_1 = random.gauss(mu=0, sigma=0.3)*0.1 #x noise
-      noise_2 =  random.gauss(mu=0.2, sigma=0.1) #y noiseA
-      noise_3 = random.gauss(mu=0.2, sigma=0.1)*0.1   #z noise
-      #Generate random position from the side(3)
-      x_safe =    (max_bound[0]+min_bound[0])/2+noise_1
-      y_safe = max_bound[1]+noise_2
-      z_safe = (max_bound[2]+min_bound[2])/2+noise_3
+        # noise for side(3)
+        noise_1 = random.gauss(mu=0, sigma=0.3)*0.1  # x noise
+        noise_2 = random.gauss(mu=0.2, sigma=0.1)  # y noiseA
+        noise_3 = random.gauss(mu=0.2, sigma=0.1)*0.1  # z noise
+        # Generate random position from the side(3)
+        x_safe = (max_bound[0]+min_bound[0])/2+noise_1
+        y_safe = max_bound[1]+noise_2
+        z_safe = (max_bound[2]+min_bound[2])/2+noise_3
 
     elif where == 4:
-      #noise for side(4)
-      noise_1 = random.gauss(mu=0.2, sigma=0.1) #x noise
-      noise_2 = random.gauss(mu=0, sigma=0.4)*0.1 #y noiseA
-      noise_3 = random.gauss(mu=0.25, sigma=0.1)*0.1   #z noise
-      #Generate random position from the side(4)
-      x_safe = max_bound[0]+noise_1
-      y_safe = (max_bound[1]+min_bound[1])/2+noise_2
-      z_safe = (max_bound[2]+min_bound[2])/2+noise_3
-      
+        # noise for side(4)
+        noise_1 = random.gauss(mu=0.2, sigma=0.1)  # x noise
+        noise_2 = random.gauss(mu=0, sigma=0.4)*0.1  # y noiseA
+        noise_3 = random.gauss(mu=0.25, sigma=0.1)*0.1  # z noise
+        # Generate random position from the side(4)
+        x_safe = max_bound[0]+noise_1
+        y_safe = (max_bound[1]+min_bound[1])/2+noise_2
+        z_safe = (max_bound[2]+min_bound[2])/2+noise_3
+
     else:
-      print("Please input number 0 ~ 4 to select the direction to generate random grasp")
+        print("Please input number 0 ~ 4 to select the direction to generate random grasp")
 
     return [x_safe, y_safe, z_safe]
 
 
-def update_gripper_direction(safe_position, gripperCID,where):
-#SELECT CORRESPONDING ORIENTATION OF GRASPING HERE (0~4) WHICH MATCHES POSITION IN FUNCTION random_position():
+def update_gripper_direction(safe_position, gripperCID, where):
+    # SELECT CORRESPONDING ORIENTATION OF GRASPING HERE (0~4) WHICH MATCHES POSITION IN FUNCTION random_position():
 
     # # -----Motion 1 : Flash-----
     # #Downward Pose
@@ -110,15 +111,20 @@ def update_gripper_direction(safe_position, gripperCID,where):
     # Current gripper pos
     current_position = [0, 0, 3]
     if where == 0:
-      hand_ori = p.getQuaternionFromEuler([0, np.pi/2, 0])  # #above the object  (0)
+        hand_ori = p.getQuaternionFromEuler(
+            [0, np.pi/2, 0])  # #above the object  (0)
     elif where == 1:
-      hand_ori = p.getQuaternionFromEuler([0,0,0])   #at side of the object (1)
+        hand_ori = p.getQuaternionFromEuler(
+            [0, 0, 0])  # at side of the object (1)
     elif where == 2:
-      hand_ori = p.getQuaternionFromEuler([np.pi,0, np.pi/2])   #at side of the object  (2)
+        hand_ori = p.getQuaternionFromEuler(
+            [np.pi, 0, np.pi/2])  # at side of the object  (2)
     elif where == 3:
-      hand_ori = p.getQuaternionFromEuler([np.pi,0, -np.pi/2])   #at side of the object (3)
+        hand_ori = p.getQuaternionFromEuler(
+            [np.pi, 0, -np.pi/2])  # at side of the object (3)
     elif where == 4:
-      hand_ori = p.getQuaternionFromEuler([0,0,np.pi])  #at side of the object  (4)
+        hand_ori = p.getQuaternionFromEuler(
+            [0, 0, np.pi])  # at side of the object  (4)
     steps = 80
     for step in range(steps):
         t = step / steps
@@ -220,14 +226,14 @@ def SaveData(position, orientation, label, obj_pos):
     data = {key: [] for key in data.keys()}
 
 
-def random_grasp_object(object_id, gripperID, gripperCID,where):
+def random_grasp_object(object_id, gripperID, gripperCID, where):
     # procedure of grasping and collect data
 
     # Generate random position around the object
-    random_Pos = random_position(object_id,where)
+    random_Pos = random_position(object_id, where)
 
     # update gripper Postion to random_Pos and predefined Orientation
-    update_gripper_direction(random_Pos,gripperCID,where)
+    update_gripper_direction(random_Pos, gripperCID, where)
 
     # Reshape pos, get ready for grasping
     reshape(gripperID)
@@ -269,20 +275,18 @@ def run_GUI_Once(where):
     cid = p.connect(p.SHARED_MEMORY)
     if (cid < 0):
         p.connect(p.GUI)
-    # setAdditionalSearchPath
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.resetSimulation()
     p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
     initial_position = [0, 0, 3]
     initial_orientation = p.getQuaternionFromEuler([0, 0, 0])
-    objects = [p.loadURDF("pr2_gripper.urdf",
+    objects = [p.loadURDF("urdf/Gripper/pr2_gripper.urdf",
                           initial_position, initial_orientation)]
     pr2_gripper = objects[0]  # Gripper ID
     # Loading Table's urdf
-    table = p.loadURDF(os.path.join(pybullet_data.getDataPath(
-    ), "table/table.urdf"), [0.0, -0.4, 0], [0, 0, 0, 1], 0)
+    table = p.loadURDF("urdf/Table/table.urdf",
+                       [0.0, -0.4, 0], [0, 0, 0, 1], 0)
     # Loading object
-    objID = p.loadURDF("example/share/cube_small.urdf",
+    objID = p.loadURDF("urdf/Object/cube_small.urdf",
                        [0.0, -0.4, 0.62], globalScaling=2)
     jointPositions = [3, 0.000000, 3, 0.000000]
     for jointIndex in range(p.getNumJoints(pr2_gripper)):
@@ -298,7 +302,7 @@ def run_GUI_Once(where):
     p.setGravity(0, 0, -10)
     os.system('cls')  # Clear terminal
     print("------------------------------Start------------------------------")
-    random_grasp_object(objID, pr2_gripper, pr2_cid,where)
+    random_grasp_object(objID, pr2_gripper, pr2_cid, where)
     p.stepSimulation()
     print("------------------------------End------------------------------")
     time.sleep(2)
@@ -306,28 +310,27 @@ def run_GUI_Once(where):
     p.disconnect()
 
 
-#No GUI DataCollection
-def NoGUI_DataCollection(where,batch_size=100):
+# No GUI DataCollection
+def NoGUI_DataCollection(where, batch_size=100):
     # --------------------Initialization Start--------------------
     # Shut down GUI
     for i in range(batch_size):
         cid = p.connect(p.DIRECT)
         if cid < 0:
             raise RuntimeError("Failed to connect to PyBullet in DIRECT mode.")
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.resetSimulation()
         p.setGravity(0, 0, -10)
         p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
         initial_position = [0, 0, 3]
         initial_orientation = p.getQuaternionFromEuler([0, 0, 0])
-        objects = [p.loadURDF("pr2_gripper.urdf",
+        objects = [p.loadURDF("urdf/Gripper/pr2_gripper.urdf",
                               initial_position, initial_orientation)]
         pr2_gripper = objects[0]  # Gripper ID
         # Loading Table's urdf
-        table = p.loadURDF(os.path.join(pybullet_data.getDataPath(
-        ), "table/table.urdf"), [0.0, -0.4, 0], [0, 0, 0, 1], 0)
+        table = p.loadURDF("urdf/Table/table.urdf",
+                           [0.0, -0.4, 0], [0, 0, 0, 1], 0)
         # Loading object
-        objID = p.loadURDF("example/share/cube_small.urdf",
+        objID = p.loadURDF("urdf/Object/cube_small.urdf",
                            [0.0, -0.4, 0.62], globalScaling=2)
         jointPositions = [3, 0.000000, 3, 0.000000]
         for jointIndex in range(p.getNumJoints(pr2_gripper)):
@@ -342,34 +345,32 @@ def NoGUI_DataCollection(where,batch_size=100):
 
         os.system('cls')  # Clear terminal
         print(f"--- Collecting Cample {i + 1} / {batch_size} ---")
-        random_grasp_object(objID, pr2_gripper, pr2_cid,where)
+        random_grasp_object(objID, pr2_gripper, pr2_cid, where)
         p.stepSimulation()
         print("--- Data Collection Completed ---")
         time.sleep(2)
         p.disconnect()
 
 
-#GUI DataCollection
-def GUI_DataCollection(where,batch_size=100):  # Recommend!
+# GUI DataCollection
+def GUI_DataCollection(where, batch_size=100):  # Recommend!
     # --------------------Initialization Start--------------------
     for i in range(batch_size):
         cid = p.connect(p.SHARED_MEMORY)
         if (cid < 0):
             p.connect(p.GUI)
-        # setAdditionalSearchPath
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.resetSimulation()
         p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
         initial_position = [0, 0, 3]
         initial_orientation = p.getQuaternionFromEuler([0, 0, 0])
-        objects = [p.loadURDF("pr2_gripper.urdf",
+        objects = [p.loadURDF("urdf/Gripper/pr2_gripper.urdf",
                               initial_position, initial_orientation)]
         pr2_gripper = objects[0]  # Gripper ID
         # Loading Table's urdf
-        table = p.loadURDF(os.path.join(pybullet_data.getDataPath(
-        ), "table/table.urdf"), [0.0, -0.4, 0], [0, 0, 0, 1], 0)
+        table = p.loadURDF("urdf/Table/table.urdf",
+                           [0.0, -0.4, 0], [0, 0, 0, 1], 0)
         # Loading object
-        objID = p.loadURDF("example/share/cube_small.urdf",
+        objID = p.loadURDF("urdf/Object/cube_small.urdf",
                            [0.0, -0.4, 0.62], globalScaling=2)
         jointPositions = [3, 0.000000, 3, 0.000000]
         for jointIndex in range(p.getNumJoints(pr2_gripper)):
@@ -388,7 +389,7 @@ def GUI_DataCollection(where,batch_size=100):  # Recommend!
         os.system('cls')  # Clear terminal
         print("------------------------------Start------------------------------")
         print(f"--- Collecting Cample {i + 1} / {batch_size} ---")
-        random_grasp_object(objID, pr2_gripper, pr2_cid,where)
+        random_grasp_object(objID, pr2_gripper, pr2_cid, where)
         p.stepSimulation()
         print("------------------------------End------------------------------")
         time.sleep(1)
@@ -397,6 +398,6 @@ def GUI_DataCollection(where,batch_size=100):  # Recommend!
 
 # ----------------------------------Run------------------------------------
 # run_GUI_Once(2)
-# NoGUI_DataCollection(batch_size=100)
-GUI_DataCollection(0,50)
+NoGUI_DataCollection(1, batch_size=100)
+# GUI_DataCollection(0,50)
 # ----------------------------------------------------------------------
